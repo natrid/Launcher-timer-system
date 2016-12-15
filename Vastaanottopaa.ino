3df99d174d3a46bb58277be532f9178b9397044f
Daniel Natri <daniel.natri@student.tut.fi>
2016-12-15 20:46:41 +0200

    Improved functions and corrected speed calculations.

---
+++ Vastaanottopaa.ino
@@ -0,0 +1,57 @@
+//Vastaanottopaa
+//Version: 0.1
+// Date: 15.12.2016
+
+boolean firstPortActivated = false;
+boolean secondPortActivated = false;
+
+void setup()
+{
+  attachInterrupt(0, firstPort, RISING);  //Interrupt for firt gate on Digital 2-pin
+  attachInterrupt(1, secondPort, RISING); //Interrupt for second gate on Digital 3-pin
+  pinMode(8, OUTPUT); //Pin 8 works as the signal to lähetyspää
+
+  pinMode(12,OUTPUT); //LED for first gate
+  pinMode(13,OUTPUT); //LED for second gate
+  //Serial.begin(9600);
+}
+
+void loop(){
+  //Wait for first port to activate
+  while(!firstPortActivated){
+    delay(1);
+  }
+  digitalWrite(8, HIGH); //START PULSE Pulse signal = HIGH
+
+  secondPortActivated = false; //Make sure second port is not yet activated
+
+  unsigned long currentTime =  millis();
+  //If crossing gates takes more than three seconds
+  //Send crossing signal to display slowest speed of 1 km/h (= 1/3 m/s)
+  unsigned long firstPortTime = millis();
+  firstPortTime += 3000;
+  while(!secondPortActivated){
+    if(currentTime > firstPortTime){
+      secondPortActivated = true;
+    }
+    currentTime = millis();
+  }
+
+  digitalWrite(8, LOW); //END PULSE Pulse signal = LOW
+  firstPortActivated = false;
+  secondPortActivated = false;
+}
+
+void firstPort(){
+  //Debug lights
+  digitalWrite(13, HIGH);
+  digitalWrite(12, LOW);
+  firstPortActivated = true;
+}
+
+void secondPort(){
+  //Debug lights
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
+  secondPortActivated = true;
+}
--- lahetyspaa.ino
+++ lahetyspaa.ino
@@ -22,8 +22,6 @@ void setup(){
   lc.clearDisplay(0);// clear screen
   initLights();
   showHello();
-  countdown();
-
   Serial.begin(9600); //Serial communications
 }
 
@@ -38,14 +36,9 @@ void loop(){
   digitalWrite(9,LOW);
   digitalWrite(8,LOW);
 
-  float buttonVoltage = 0.0;
-  float buttonValue = 0.0;
   //Start button that is connected to A0
   //When voltage is higher than 2.5V button was pressed
-  do{
-    buttonValue = analogRead(A0);
-    buttonVoltage = buttonValue * (5.0/1023.0);
-  }while(buttonVoltage <= 2.5);
+  while (!buttonRead(0));
 
   //Start countdown
   countdown();
@@ -56,7 +49,8 @@ void loop(){
   firstPortTime = 0;
   secondPortTime = 0;
 
-  //Do if both ports are activated at the same time
+  //Do while both gates have the same value
+  //Starting condition is that both gates have values "false"
   do{
     firstPortActivated = false;
     secondPortActivated = false;
@@ -68,31 +62,21 @@ void loop(){
       timeElapsed = millis();
       timeElapsed = timeElapsed - startTime;
       timeElapsed = timeElapsed / 1000;
-      showTime(timeElapsed);
+      displayTime(timeElapsed);
+      //showTime(timeElapsed);
       //Check if stop button (A1) is pressed
-      buttonValue = analogRead(A1);
-      buttonVoltage = buttonValue * (5.0/1023.0);
-      if(buttonVoltage >= 2.5){
+      if(buttonRead(1)){
         return;
       }
     }
 
-    //Set false so second port activation before first port is ignored
+    //Ensure that second port has not activated
     secondPortActivated = false;
 
     //Wait for second port to be activated
     while(secondPortActivated == false){
-      //Show new time once every 10 ms
-      /*delay(10);
-      timeElapsed = millis();
-      timeElapsed = timeElapsed - startTime;
-      timeElapsed = timeElapsed / 1000;
-      showTime(timeElapsed);
-      */
       //Check if stop button (A1) is pressed
-      buttonValue = analogRead(A1);
-      buttonVoltage = buttonValue * (5.0/1023.0);
-      if(buttonVoltage >= 2.5){
+      if(buttonRead(1)){
         return;
       }
     }
@@ -101,13 +85,13 @@ void loop(){
   digitalWrite(8,LOW);
   delay(10);
 
-  //Only if port activation times are different then show time and speed
+  //If recorded times for gate activation are different calculate speed
   if(secondPortTime > firstPortTime){
     //Calculate travel time
     //Time between start and first port in seconds
     double travelTime = firstPortTime - startTime;
     travelTime /= 1000;
-    showTime(travelTime);
+    displayTime(travelTime);
 
 
     double timeDifference = secondPortTime - firstPortTime;
@@ -121,48 +105,32 @@ void loop(){
   secondPortActivated = false;
 }
 
-//Function for displaying time on screen
-void showTime(double& time){
-  int temp;
-  //Time calculations
-  //Jos tulos on > 99.99 niin otetaan 100:n jakojäännös
-  if(time > 99.99){
-    temp = (int)time / 100;
-    time = time - (temp*100);
-    lc.setDigit(0,3,temp,0);
+//Function for displaying time in seconds
+//time given in seconds
+void displayTime(double& time){
+  //lc.clearDisplay(0);
+  int t1 = (int)(time*100)%10; // 1/100
+  int t2 = (int)(time*10)%10;  // 1/10
+  int t3 = (int)time%10;       // 1
+  int t4 = (int)(time/10)%10;  // 10
+  int t5 = (int)(time/100)%10; // 100
 
-    int timeInt = time /10;
-    lc.setDigit(0,2,timeInt,0);
-    timeInt = time - (timeInt * 10);
-    if(timeInt >= 0 && timeInt < 10){
-      //Serial.println(timeInt);
-      lc.setDigit(0,1,timeInt,1);
+
+  //Handle display so that four digits are displayed correctly
+  if (t5 == 0) {
+    lc.setDigit(0,0,t1,0);
+    lc.setDigit(0,1,t2,0);
+    lc.setDigit(0,2,t3,1);
+    if (t4 != 0) {
+      lc.setDigit(0,3,t4,0);
     }
-
-    timeInt = (time - (int)time ) * 100;
-    temp = timeInt / 10;
-    //Serial.println(temp);
-    lc.setDigit(0,0,temp,0);
   }
   else{
-    int timeInt = time /10;
-    //Serial.println(timeInt);
-    lc.setDigit(0,3,timeInt,0);
-    timeInt = time - (timeInt * 10);
-    if(timeInt >= 0 && timeInt < 10){
-      //Serial.println(timeInt);
-      lc.setDigit(0,2,timeInt,1);
-    }
-
-    timeInt = (time - (int)time ) * 100;
-    temp = timeInt / 10;
-    //Serial.println(temp);
-    lc.setDigit(0,1,temp,0);
-    temp = timeInt % 10;
-    //Serial.println(temp);
-    lc.setDigit(0,0,temp,0);
+    lc.setDigit(0,0,t2,0);
+    lc.setDigit(0,1,t3,1);
+    lc.setDigit(0,2,t4,0);
+    lc.setDigit(0,3,t5,0);
   }
-
 }
 
 //Shows speed that is given by parameter speed in km/h
@@ -276,4 +244,24 @@ double calculateSpeed(double& timeDifference){
   double distance = 0.001; //Distance between sensors in km
   double speed = distance / timeDifference;
   return speed;
+}
+
+bool buttonRead(int button){
+  float buttonVoltage = 0.0;
+  float buttonValue = 0.0;
+  switch (button) {
+    case 0:
+      buttonValue = analogRead(A0);
+      break;
+    case 1:
+      buttonValue = analogRead(A1);
+      break;
+    default:
+      return false;
+  }
+  buttonVoltage = buttonValue * (5.0/1023.0);
+  if(buttonVoltage >= 2.5){
+    return true;
+  }
+  return false;
 }
